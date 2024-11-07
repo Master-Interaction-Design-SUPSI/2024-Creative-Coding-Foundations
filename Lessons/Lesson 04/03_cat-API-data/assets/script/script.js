3b2ca44281802236959fd8845528c6046fd0b1c8
giovannipro <g.profet@gmail.com>
2024-11-07 17:24:02 +0100

    add the webpage that displays cats from the cap API

---
+++ Lessons/Lesson 04/03_cat-API-data/assets/script/script.js
@@ -0,0 +1,25 @@
+const API_URL = "https://api.thecatapi.com/v1/images/search"
+const CONTAINER = document.getElementById("container")
+
+fetch(API_URL)
+  .then(response => response.json()) 
+  .then(data => display_data(data))
+  .catch(error => display_error(error));
+
+function display_error(error){
+    console.error('Error:', error)
+    CONTAINER.innerHTML = "I'm sorry. The cat is not available."
+}
+
+function display_data(data){
+    console.log(data)
+
+    let output = ""
+    let cat_obj = data[0]
+
+    output +=  `
+        <img src="${cat_obj.url}" width="${cat_obj.width}" height="${cat_obj.height}" class="cat_image" />
+    `
+
+    CONTAINER.innerHTML = output;
+}
