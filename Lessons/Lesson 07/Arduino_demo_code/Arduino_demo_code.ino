c8db4f9655fd77c20f2d867b7ecda3e77c1ba9cb
marclura <29801332+marclura@users.noreply.github.com>
2024-11-28 10:21:35 +0100

    update arduino sketch

--- Lessons/Lesson 07/Arduino_demo_code/Arduino_demo_code.ino
+++ Lessons/Lesson 07/Arduino_demo_code/Arduino_demo_code.ino
@@ -14,6 +14,9 @@ void loop() {
   pot2 = analogRead(A1);  // read the potentiometer on pin A1, values are from 0 to 1023
   pot3 = analogRead(A2);  // read the potentiometer on pin A2, values are from 0 to 1023
 
+  button = digitalRead(7);
+
+
   // send the data in a CSV (Comma Separated Values) format to the computer
   Serial.println(String(pot1) + "," + String(pot2) + "," + String(pot3));
 
