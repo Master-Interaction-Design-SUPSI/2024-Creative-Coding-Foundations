35526e1cdba04174632e3a2c7be49f37bbd16c98
Marco De Martino <marco.dema2001@gmail.com>
2024-12-05 18:10:51 +0100

    Add files via upload

---
+++ Exercise_final/Marco_DeMartino/assets/classes/CollisionBlock.js
@@ -0,0 +1,25 @@
+class CollisionBlock {
+  constructor({ position }) {
+    this.position = position;
+    this.width = 64;
+    this.height = 64;
+  }
+
+  draw() {
+    c.fillStyle = "rgba(255, 0, 0, 0.0)";
+    c.fillRect(this.position.x, this.position.y, this.width, this.height);
+  }
+}
+
+class trapBlock {
+  constructor({ position }) {
+    this.position = position;
+    this.width = 64;
+    this.height = 64;
+  }
+
+  draw() {
+    c.fillStyle = "rgba(255, 0, 0, 0.0)";
+    c.fillRect(this.position.x, this.position.y, this.width, this.height);
+  }
+}
